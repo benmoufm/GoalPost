455a1d1d50e069c30bd38ea272d8cf132713c12d
Melodie Benmouffek <m.benmouffek@gmail.com>
2018-01-24 16:55:33 +0100

    Create UIButton extension to change selected and unselected color

---
+++ GoalPost/Extensions/UIButtonExtension.swift
@@ -0,0 +1,19 @@
+//
+//  UIButtonExtension.swift
+//  GoalPost
+//
+//  Created by Mélodie Benmouffek on 24/01/2018.
+//  Copyright © 2018 Mélodie Benmouffek. All rights reserved.
+//
+
+import UIKit
+
+extension UIButton {
+    func setSelectedColor() {
+        backgroundColor = #colorLiteral(red: 0.4922404289, green: 0.7722371817, blue: 0.4631441236, alpha: 1)
+    }
+
+    func setDeselectedColor() {
+        backgroundColor = #colorLiteral(red: 0.7464011312, green: 0.8857318759, blue: 0.7402122021, alpha: 1)
+    }
+}
