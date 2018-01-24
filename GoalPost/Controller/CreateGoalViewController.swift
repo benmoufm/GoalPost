262873c425203e2d5f08417ff49dcc84ea695cb0
Melodie Benmouffek <m.benmouffek@gmail.com>
2018-01-24 16:13:22 +0100

    Create create goal view controller and add outlets and actions

---
+++ GoalPost/Controller/CreateGoalViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CreateGoalViewController.swift
+//  GoalPost
+//
+//  Created by Mélodie Benmouffek on 24/01/2018.
+//  Copyright © 2018 Mélodie Benmouffek. All rights reserved.
+//
+
+import UIKit
+
+class CreateGoalViewController: UIViewController {
+    //MARK: - Outlets
+    @IBOutlet weak var goalTextView: UITextView!
+    @IBOutlet weak var shortTermButton: UIButton!
+    @IBOutlet weak var longTermButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    //MARK : - Actions
+    @IBAction func longTermButtonPressed(_ sender: Any) {
+    }
+
+    @IBAction func shortTermButtonPressed(_ sender: Any) {
+    }
+
+    @IBAction func nextButtonPressed(_ sender: Any) {
+    }
+
+    @IBAction func backButtonPressed(_ sender: Any) {
+    }
+}
